16a42a93a7f1d007aae6dfd947f96c97e9ea3a6e
kos-zenin <kos.zenin@gmail.com>
2020-03-11 17:32:06 +0000

    Introduce results reporter Raise argument error if no file given to parse function

--- bin/parser.rb
+++ bin/parser.rb
@@ -1,6 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def parse(file:); end
+DEFAULT_PARSER = ->() {}
+def parse(file:, parser: DEFAULT_PARSER)
+  raise ArgumentError, "Usage: #{$0} FILENAME" if file.nil?
+end
 
-parse(file: ARGV[0])
+DEFAULT_REPORTER = ->() {}
+def report(results, reporter: DEFAULT_REPORTER)
+end
+
+if $0 == __FILE__
+  parse(file: ARGV[0]).then { |results| report(results) }
+end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -11,15 +11,19 @@ describe 'parse' do
 
   context 'when file passed' do
     let(:file) { instance_double('file') }
-    let(:expected_result) do
-      {
-        '1.1.1.1' => 10,
-        '2.2.2.2' => 5
-      }
-    end
+    let(:test_parser) { ->(file) { file } }
 
     it 'parses file, builds history of visits in descending order' do
-      expect(parse(file: file)).to eq(expected_result)
+      expect(parse(file: file, parser: test_parser)).to eq(file)
     end
+  end
+end
+
+describe 'report' do
+  let(:sample_results) { "test results" }
+  let(:test_reporter) { ->(results) { results } }
+
+  it 'calls reporter' do
+    expect(report(sample_results, reporter: test_reporter)).to eq(sample_results)
   end
 end
