5dceada557920c1cd443aaded584db3adc479998
kos-zenin <kos.zenin@gmail.com>
2020-03-12 10:15:53 +0000

    Install simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@
 require_relative './../config/boot'
 require 'rspec/its'
 
+require 'simplecov'
+SimpleCov.start
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
