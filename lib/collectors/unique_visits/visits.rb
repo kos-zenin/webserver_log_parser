7e99c33fd6f190ffb5769df135af8d795d8d3f4c
kos-zenin <kos.zenin@gmail.com>
2020-03-12 09:21:55 +0000

    Implement unique visits class

---
+++ lib/collectors/unique_visits/visits.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Collectors
+  module UniqueVisits
+    class Visits
+      def initialize(sorter: ::Collectors::UniqueVisits::Sorter.new)
+        @sorter = sorter
+        @visits = {}
+      end
+
+      def add(visit:)
+        @visits[visit.route] ||= ::Collectors::UniqueVisits::Visit.new(route: visit.route)
+
+        @visits[visit.route].increment_visit
+      end
+
+      def visits
+        @sorter.call(visits: @visits.values)
+      end
+    end
+  end
+end
