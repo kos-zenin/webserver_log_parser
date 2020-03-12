bf286e76a9b3d9cab8c0085f62934a18a2baaa85
kos-zenin <kos.zenin@gmail.com>
2020-03-12 11:27:21 +0000

    Update specs of line parser to use row data structure validation

--- spec/lib/files/parsers/logs/line_spec.rb
+++ spec/lib/files/parsers/logs/line_spec.rb
@@ -6,14 +6,24 @@ describe ::Files::Parsers::Logs::Line do
   describe 'call' do
     context 'when line is correct' do
       let(:line) { '/test/2 902.1.12.0' }
+      let(:row) { instance_double(::Files::Data::Logs::Row, valid?: true) }
+
+      before do
+        expect(::Files::Data::Logs::Row).to receive(:new).with('/test/2', '902.1.12.0').and_return(row)
+      end
 
       it 'returns url, ip' do
-        expect(subject.call).to eq(['/test/2', '902.1.12.0'])
+        expect(subject.call).to eq(row)
       end
     end
 
     context 'when line is mailformed' do
       let(:line) { 'invalid line' }
+      let(:row) { instance_double(::Files::Data::Logs::Row, valid?: false) }
+
+      before do
+        expect(::Files::Data::Logs::Row).to receive(:new).with('invalid', 'line').and_return(row)
+      end
 
       it 'returns empty value' do
         expect(subject.call).to be_nil
