ebb4c2e8be748f284c481b71f2feafae453bd1a8
kos-zenin <kos.zenin@gmail.com>
2020-03-12 11:21:19 +0000

    Implement validation for row data structure

--- lib/files/data/logs/row.rb
+++ lib/files/data/logs/row.rb
@@ -4,7 +4,12 @@ module Files
   module Data
     module Logs
       class Row < Struct.new(:route, :ip)
-        def valid?; end
+        ROUTE_REGEX = %r{^/\w+(/\w+)*$}.freeze
+        IP_REGEX = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.freeze
+
+        def valid?
+          route =~ ROUTE_REGEX && ip =~ IP_REGEX
+        end
       end
     end
   end
