2ce1c3efc277630ba39276495dd8cfca94666c3e
kos-zenin <kos.zenin@gmail.com>
2020-03-12 11:29:56 +0000

    Update specs of file parser to use new line parser interface

--- spec/lib/files/parsers/logs/file_spec.rb
+++ spec/lib/files/parsers/logs/file_spec.rb
@@ -9,7 +9,7 @@ describe ::Files::Parsers::Logs::File do
   describe 'call' do
     let(:expected_rows) { [valid_row] }
     let(:valid_row) { instance_double(::Files::Data::Logs::Row) }
-    let(:line_parser) { instance_double(::Files::Parsers::Logs::Line, call: ['/test', '1.1.1.1']) }
+    let(:line_parser) { instance_double(::Files::Parsers::Logs::Line, call: valid_row) }
 
     let(:valid_line) { '/test 1.1.1.1' }
 
@@ -17,7 +17,6 @@ describe ::Files::Parsers::Logs::File do
       expect(test_file_reader).to receive(:call).with(file: file).and_yield(valid_line)
 
       expect(::Files::Parsers::Logs::Line).to receive(:new).with(line: valid_line).and_return(line_parser)
-      expect(::Files::Data::Logs::Row).to receive(:new).with(route: '/test', ip: '1.1.1.1').and_return(valid_row)
     end
 
     it 'iterates over log lines and yields row' do
