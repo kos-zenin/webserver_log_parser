90848706941167d80e8899c6e08c9eb3ba461299
kos-zenin <kos.zenin@gmail.com>
2020-03-12 10:14:42 +0000

    Add support for plural\singular 'view' word

--- lib/collectors/unique_visits/visit_decorator.rb
+++ lib/collectors/unique_visits/visit_decorator.rb
@@ -3,14 +3,24 @@
 module Collectors
   module UniqueVisits
     class VisitDecorator
-      DEFAULT_FORMAT = '%<route>s %<count>s unique views'
+      VIEW_KEYWORD = 'view'
+      VIEW_KEYWORD_PLURAL = 'views'
+      VIEW_KEYWORD_PLURAL_MAPPING = {
+        1 => VIEW_KEYWORD
+      }.freeze
+      DEFAULT_FORMAT = '%<route>s %<count>s unique %<view_text>s'
 
       def initialize(format: DEFAULT_FORMAT)
         @format = format
       end
 
       def call(visit:)
-        format(@format, route: visit.route, count: visit.count)
+        format(
+          @format,
+          route: visit.route,
+          count: visit.count,
+          view_text: VIEW_KEYWORD_PLURAL_MAPPING.fetch(visit.count, VIEW_KEYWORD_PLURAL)
+        )
       end
     end
   end
