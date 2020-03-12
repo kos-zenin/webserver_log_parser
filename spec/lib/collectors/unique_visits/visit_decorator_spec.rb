5b2f294647c7bcd924a193cba2f9af6d7b18d976
kos-zenin <kos.zenin@gmail.com>
2020-03-12 10:07:16 +0000

    Add spec for decorator when only 1 view occured

--- spec/lib/collectors/unique_visits/visit_decorator_spec.rb
+++ spec/lib/collectors/unique_visits/visit_decorator_spec.rb
@@ -3,11 +3,20 @@
 describe ::Collectors::UniqueVisits::VisitDecorator do
   subject { described_class.new }
 
-  let(:visit) { instance_double(::Collectors::UniqueVisits::Visit, route: '/test', count: 100) }
+  let(:visit) { instance_double(::Collectors::UniqueVisits::Visit, route: '/test', count: count) }
+  let(:count) { 100 }
 
   describe 'call' do
     it 'formats visit' do
       expect(subject.call(visit: visit)).to eq('/test 100 unique views')
+    end
+
+    context "when 1 view" do
+      let(:count) { 1 }
+
+      it 'formats visit' do
+        expect(subject.call(visit: visit)).to eq('/test 1 unique view')
+      end
     end
   end
 end
